a51dc97e8e396b6600769abbbc7ad86ec0d09d70
Björn Westergard <bjorn.westergard@lanetix.com>
2016-01-25 15:05:06 -0800

    factoring out
    Signed-off-by: Björn Westergard <bjorn.westergard@lanetix.com>

--- lensfun.js
+++ lensfun.js
@@ -1,41 +1,76 @@
+'use strict';
+
+const moment = require('moment')
 const R = require("ramda")
 
 const lensWhere = R.curry(
-  (listLens, lensPredicate, structure) => {
-    const list = R.view(listLens, structure)
-    for (var i = 0; i < list.length; i++) {
-      var lens = lensPredicate(list[i])
-      if (lens === true) {
-        return R.compose(listLens, R.lensIndex(i))
-      } else if (R.type(lens) === 'Function') {
-        return R.compose(listLens, R.lensIndex(i), lens)
+  (listLens, lensPredicate, structure) => R.pipe(
+    R.view(listLens),
+    (list) => {
+      for (var i = 0; i < list.length; i++) {
+        let lens = lensPredicate(list[i])
+        if (lens === true) {
+          return R.compose(listLens, R.lensIndex(i))
+        } else if (R.type(lens) === 'Function') {
+          return R.compose(listLens, R.lensIndex(i), lens)
+        }
       }
+      return undefined;
     }
-    return undefined;
-  }
+  )(structure)
 )
 
-const foo = {
-  people: [
+const defaultState = {
+  speakers: [
     {
       name: 'Bjorn',
-      vices: ['sloth', 'greed']
+      speeches: [
+        {
+          'start': '2016-01-15T19:15:33-05:00',
+          'end': '2016-01-15T19:35:44-05:00'
+        }
+      ],
+      onStack: false,
+      approved: true
+    },
+    {
+      name: 'Bob',
+      speeches: [
+        {
+          'start': '2016-01-15T19:15:33-05:00'
+        },
+        {
+          'start': '2016-01-12T19:15:33-05:00',
+          'end': '2016-01-12T19:35:44-05:00'
+        }
+      ],
+      onStack: false,
+      approved: true
     },
     {
       name: 'Sven',
-      vices: ['lust', 'pride']
+      speeches: [{
+        'start': '2016-01-15T19:15:13-05:00',
+        'end': '2016-01-15T19:35:54-05:00'
+      }, {
+        'start': '2016-01-17T19:15:13-05:00',
+        'end': '2016-01-17T19:35:14-05:00'
+      }],
+      onStack: false,
+      approved: false
     }
   ]
 }
 
-const prideFromTop = lensWhere(
-  R.lensProp('people'),
-  lensWhere(R.lensProp('vices'), R.equals('pride')),
-  foo
+const speaker = lensWhere(
+  R.lensProp('speakers'),
+  lensWhere(
+    R.lensProp('speeches'),
+    R.complement(R.has('end'))
+  ),
+  defaultState
 )
 
-if (prideFromTop !== undefined) {
-  console.log(R.view(prideFromTop, foo))
-} else {
-  console.log('not found')
-}
+console.log(
+  JSON.stringify(R.over(speaker, R.assoc('end', moment().format()), defaultState), null, 2)
+)
